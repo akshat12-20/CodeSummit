9e9baf96f3b4819583e81b27276e9c8b2e3c91f0
akshat12-20 <akshatbhardwaj400@gmail.com>
2025-07-04 15:57:27 +0530

    feat(auth): add register controller with JWT authentication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcryptjs";
 import {db} from "../libs/db.js"
+import { UserRole } from "../generated/prisma/index.js";
+import jwt from "jsonwebtoken";
 
 
 export const register= async (req, res)=>{
@@ -18,9 +20,41 @@ export const register= async (req, res)=>{
         }
 
         const hashedPassword= await bcrypt.hash(password, 10);
+
+        const newUser = await db.user.create({
+            data:{
+                email,
+                password: hashedPassword,
+                name,
+                role: UserRole.USER
+            }
+        })
+
+        const token= jwt.sign({id:newUser.id}, process.env.JWT_SECRET, {expiresIn:"7d"})
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV!== "development",
+            maxAge: 1000 * 60 * 60 * 24 * 7 //7 days
+        })
+
+        res.status(201).json({
+            success: true,
+            message:"User created successfully.",
+            user:{
+                id:newUser.id,
+                email:newUser.email,
+                role: newUser.role,
+                image:newUser.image
+            }
+        })
+
         
     } catch (error) {
-        
+        console.error("Error creating user: ", error);
+        res.status(500).json({
+            error:"Error creating user."
+        })
     }
 
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,18 @@
 import express, { response } from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser"
+
+
 import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 const port= process.env.PORT || 3000 ;
 
 const app= express();
+
 app.use(express.json());
+app.use(cookieParser());
+
 
 app.get('/', (req,res)=>{
     res.send("Welcome to CodeSummit🔥");
